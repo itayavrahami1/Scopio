7adde56b13757170a7b48f55db3af77648a720ca
ItayAvrahami <itayavrahami1@gmail.com>
2021-02-22 16:08:22 +0200

    more

--- main.py
+++ main.py
@@ -18,25 +18,11 @@ if __name__ == '__main__':
 
         self_corr_img = scipy.signal.fftconvolve(gray1, gray1[::-1,::-1], mode='same')
         corr_img = scipy.signal.fftconvolve(gray1, gray2[::-1,::-1], mode='same')
-    # corr_img = scipy.signal.correlate2d(gray1, gray2)
 
         max_intensity = (np.unravel_index(np.argmax(self_corr_img), self_corr_img.shape))
         dx_pixel = max_intensity[1] - (np.unravel_index(np.argmax(corr_img), corr_img.shape)[1])
 
-        # print('SELF: ', max_intensity, '\nCORR ', dx_pixel,'\n----------\n')
-
-        # max_intensity = (np.unravel_index(np.argmax(self_corr_img), self_corr_img)[1])
-        # dx_pixel = (np.unravel_index(np.argmax(corr_img), corr_img.shape)[1])
-
         dz_um = dx_pixel*0.24/(2*np.tan(0.34))
-    #
         dz.append(dz_um)
-        # dz.append(dz_um - (687 * 0.24))
-    #
+
     print(dz)
-
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
