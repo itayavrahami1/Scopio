c322bfe99356efb89cc748d4b83aa125af5e3d16
ItayAvrahami <itayavrahami1@gmail.com>
2021-02-23 14:50:46 +0200

    plotting + os.scandir

--- inventory/Scripts/Service/Image/image_service.py
+++ inventory/Scripts/Service/Image/image_service.py
@@ -1,10 +1,14 @@
 import numpy as np
 import cv2 as cv
+import os
 
 
-def load_images(num):
+def load_images(img_path, files):
 
-    img1 = cv.imread(f'Interview/Interview/{num}/im1.1028x687.jpg')
-    img2 = cv.imread(f'Interview/Interview/{num}/im2.1028x687.jpg')
+    im1_path = os.path.join(img_path, files[0])
+    im2_path = os.path.join(img_path, files[1])
+
+    img1 = cv.imread(im1_path)
+    img2 = cv.imread(im2_path)
 
     return img1, img2
--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import scipy.signal
 import cv2 as cv
+import os
 
 from inventory.Scripts.Service.Image import image_service
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
+    path = r'C:\Users\itay.a\Coding\OpenCV\Interview\Interview'
     dx_pixel = []
     dz = []
+    labels = []
 
-    for i in range(10):
-        img1, img2 = image_service.load_images(i)
+    for curr_dir in os.scandir(path):
 
-        gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
-        gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+        try:
+            label = int(curr_dir.name)
+            labels.append(label)
+            files = os.listdir(os.path.join(path,curr_dir))
+            img1, img2 = image_service.load_images(os.path.join(path,curr_dir), files)
 
-        self_corr_img = scipy.signal.fftconvolve(gray1, gray1[::-1,::-1], mode='same')
-        corr_img = scipy.signal.fftconvolve(gray1, gray2[::-1,::-1], mode='same')
+            gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+            gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
-        max_intensity = (np.unravel_index(np.argmax(self_corr_img), self_corr_img.shape))
-        dx_pixel = max_intensity[1] - (np.unravel_index(np.argmax(corr_img), corr_img.shape)[1])
+            self_corr_img = scipy.signal.fftconvolve(gray1, gray1[::-1, ::-1], mode='same')
+            corr_img = scipy.signal.fftconvolve(gray1, gray2[::-1,::-1], mode='same')
 
-        dz_um = dx_pixel*0.24/(2*np.tan(0.34))
-        dz.append(dz_um)
+            max_intensity = (np.unravel_index(np.argmax(self_corr_img), self_corr_img.shape))
+            dx_pixel = max_intensity[1] - (np.unravel_index(np.argmax(corr_img), corr_img.shape)[1])
 
-    print(dz)
+            dz_um = dx_pixel*0.24/(2*np.tan(0.34))
+            dz.append(dz_um)
+        except:
+            continue
+
+    plt.plot(labels, dz, marker='*')
+    plt.title('dz [um] Vs. image label')
+    plt.grid()
+    plt.xlabel('Label')
+    plt.ylabel('dz [um]')
+    plt.show()
